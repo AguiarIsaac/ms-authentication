ea597ddc796e18d728f2d4316496b90bf4d90051
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-06-10 12:26:26 +0000

    configuração minima

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello World");
+import  express, { Request, Response, NextFunction }  from "express";
+
+const app = express();
+
+app.get('/status', ( req: Request, res: Response, next: NextFunction ) => {
+    res.status(200).send({ foo: 'Deu Certo! eu sou top'})
+});
+
+app.listen(3000, () => {
+    console.log('Aplicação rodando na porta 3000');
+});
